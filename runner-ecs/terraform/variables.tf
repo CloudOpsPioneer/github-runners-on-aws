1942a405313b54f20cd5e81d66be9f1fb4b6bcb8
EC2 Default User <ec2-user@ip-10-74-49-137.ec2.internal>
2024-10-11 00:47:33 +0000

    [runner-ecs] : Adding tf files

--- runner-ecs/terraform/variables.tf
+++ runner-ecs/terraform/variables.tf
@@ -1,15 +1,13 @@
 
 variable "vpc_id" {
   type = string
-  #default     = "vpc-05b4f2e13b7df5467"
-  default     = "vpc-01252ba88fe00818d"
+  default     = "vpc-05b4f2e13b7df5467"
   description = "VPC ID"
 }
 
 variable "private_subnet_id" {
   description = "private subnet id"
-  default     = "subnet-042cbd3c7d727dc8e"
-  #default     = "subnet-03639446afdabc019"
+  default     = "subnet-03639446afdabc019"
 }
 
 variable "region" {
