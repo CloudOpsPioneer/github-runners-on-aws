6e0e13f259e3b92824ee4c311dadc31908afecf5
EC2 Default User <ec2-user@ip-172-31-24-245.ec2.internal>
2024-10-11 03:00:43 +0000

    adding folders for ec2 methods

---
+++ runner-ec2-docker/entrypoint.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Check if the PAT is available
+if [ -z "$PAT" ]; then
+  echo "Personal Access Token not provided. Set the PAT environment variable."
+  exit 1
+fi
+
+# Fetch the runner token from GitHub API
+REGISTRATION_TOKEN=$(curl -sX POST -H "Authorization: token $PAT" "https://api.github.com/repos/${GITHUB_OWNER}/${GITHUB_REPO}/actions/runners/registration-token" | jq -r '.token')
+
+# Exit if unable to fetch token
+if [ -z "$REGISTRATION_TOKEN" ]; then
+  echo "Failed to fetch registration token"
+  exit 1
+fi
+
+# Configure the GitHub Actions Runner
+echo "Registering the GitHub Runner..."
+./config.sh --url "https://github.com/${GITHUB_OWNER}/${GITHUB_REPO}" \
+            --token "$REGISTRATION_TOKEN" \
+            --name "$RUNNER_NAME" \
+            --labels "$RUNNER_LABELS" \
+            --unattended --replace
+echo "Runner registered successfully."
+
+# Run the GitHub Actions Runner
+echo "Starting GitHub Runner..."
+exec "./run.sh"
