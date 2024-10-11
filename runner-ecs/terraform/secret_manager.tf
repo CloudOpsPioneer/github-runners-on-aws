7d94db90412cdddb0a16499164cb22da828aaae4
karthikrajkkr <34535249+karthikrajkkr@users.noreply.github.com>
2024-10-10 23:05:54 -0400

    adding comments

--- runner-ecs/terraform/secret_manager.tf
+++ runner-ecs/terraform/secret_manager.tf
@@ -6,6 +6,6 @@ resource "aws_secretsmanager_secret" "github_runner_secret" {
 resource "aws_secretsmanager_secret_version" "github_creds" {
   secret_id = aws_secretsmanager_secret.github_runner_secret.id
   secret_string = jsonencode({
-    PAT = ""
+    PAT = ""                                                    # Update it post creation. Next terraform apply will not revert back to empty value. 
   })
 }
